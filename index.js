c85940d70dcb0d4f4ce0e14a2bf584a2835a2dc0
marufbroh <marufmd775@gmail.com>
2023-06-03 00:20:43 +0600

    delete api implemented

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -58,11 +58,19 @@ async function run() {
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     });
-    
+
     app.post("/carts", async (req, res) => {
       const item = req.body;
       // console.log(item);
       const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
 
